2633ac3c55b18b235ee5fe71e30c2ee090f0d644
Sienna <smcollette@mta.ca>
2018-11-22 16:34:59 -0400

    preliminary update

--- tests/FloweringTreeTest.java
+++ tests/FloweringTreeTest.java
@@ -4,17 +4,21 @@ import org.junit.jupiter.api.Test;
 
 class FloweringTreeTest {
 
+	FloweringTree ft=new FloweringTree("Franklin");
+	
 	@Test
     void testGetName(){
-       
+       assertEquals("Franklin",ft.getName());
     }
 	@Test
     void testGetBlossomState(){
-        
+       assertEquals(false, ft.getBlossomState());
     }
 
 	@Test
     void testSetBlossomState(){
-       
+		boolean state=true;
+		ft.setBlossomState(state, new BloomReport());
+		assertEquals(state, ft.getBlossomState());
     }
-}
+}//FloweringTreeTest
